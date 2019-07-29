31419340515f1334d5e9f33339d3d7a29ef2435a
LoadingByte <hello@loadingbyte.com>
2019-07-30 00:59:12 +0200

    Enable reconfiguration of the application root (useful for reverse proxies)

--- auxiliary/webapp/__init__.py
+++ auxiliary/webapp/__init__.py
@@ -2,6 +2,13 @@ import logging
 import sys
 
 from flask import Flask
+from werkzeug.wsgi import DispatcherMiddleware
+
+
+def app_root_404(env, resp):
+    resp("404", [("Content-Type", "text/plain")])
+    return [b"404 The application root has been reconfigured."]
+
 
 # Logging config
 logging.basicConfig(level=logging.INFO,
@@ -15,6 +22,10 @@ app.config["WTF_CSRF_ENABLED"] = False
 # Load config from disk
 app.config.from_pyfile("settings.cfg", silent=True)
 app.config.from_envvar("SD_SETTINGS", silent=True)
+
+# Change the application root if configured
+if "APPLICATION_ROOT" in app.config and app.config["APPLICATION_ROOT"] != "/":
+    app.wsgi_app = DispatcherMiddleware(app_root_404, {app.config["APPLICATION_ROOT"]: app.wsgi_app})
 
 # Load data
 from . import data
